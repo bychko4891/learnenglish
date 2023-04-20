aaae763b3053cac3e7d7c468e50b8187ff24520a
Anatolii <106445100+bychko4891@users.noreply.github.com>
2023-04-20 18:57:00 +0300

    six

--- src/main/java/com/example/learnenglish/repository/TranslationPairRepository.java
+++ src/main/java/com/example/learnenglish/repository/TranslationPairRepository.java
@@ -10,16 +10,10 @@ import java.util.List;
 
 @Repository
 public interface TranslationPairRepository extends CrudRepository<TranslationPair, Integer> {
-//    @Query("SELECT CASE WHEN COUNT(lt) > 0 THEN true ELSE false END FROM TranslationPair lt WHERE lt.user.id = :userId AND lt.lesson.id = :lessonId AND lt.engText = :engText AND lt.ukrText = :ukrText")
-//@Query("SELECT CASE WHEN COUNT(lt) >  THEN true ELSE false END FROM TranslationPair lt WHERE lt.user.id = :userId AND lt.lesson.id = :lessonId AND UPPER(lt.engText) = UPPER(:engText) AND UPPER(lt.ukrText) = UPPER(:ukrText)")
-//@Query("SELECT COUNT(lt) FROM TranslationPair lt WHERE lt.user.id = :userId AND lt.lesson.id = :lessonId AND UPPER(lt.engText) = UPPER(:engText) AND UPPER(lt.ukrText) = UPPER(:ukrText)")
 
     @Query("SELECT CASE WHEN COUNT(lt) > 0 THEN true ELSE false END FROM TranslationPair lt WHERE lt.user.id = :userId AND lt.lesson.id = :lessonId AND LOWER(lt.engText) = LOWER(:engText)")
-//    @Query("SELECT COUNT(*) FROM TranslationPair t WHERE t.user.id = :userId AND t.lesson.id = : lessonId AND LOWER(t.engText) = LOWER(:engText) AND LOWER(t.ukrText) = LOWER(:ukrText)")
-   boolean existsByEngTextAndUkrText(@Param("engText") String engText, @Param("lessonId") Long lessonId, @Param("userId") Long userId);
-//    @Query("SELECT CASE WHEN COUNT(tp) > 0 THEN TRUE ELSE FALSE END FROM TranslationPair tp WHERE tp.user.id = :userId AND tp.lesson.id = :lessonId AND tp.engText = :engText AND tp.ukrText = :ukrText")
-//    boolean existsByEngTextAndUkrText(@Param("engText") String engText, @Param("ukrText") String ukrText, @Param("lessonId") Long lessonId, @Param("userId") Long userId);
-//    boolean existsByUserAndLessonIdAndEngTextAndUkrText(String engText, String ukrText, Long lessonId, Long user_id);
+    boolean existsByEngTextAndUkrText(@Param("engText") String engText, @Param("lessonId") Long lessonId, @Param("userId") Long userId);
+
     @Query("SELECT e.id FROM TranslationPair e WHERE e.user.id = :userId AND e.lesson.id = :lessonId")
     List<Long> findAllIdsByUserAndLesson(@Param("userId") Long userId, @Param("lessonId") Long lessonId);
 
