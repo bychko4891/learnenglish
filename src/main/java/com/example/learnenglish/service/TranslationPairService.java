ce231100a8e8eeb295c7c14e6fb203bd8dd5fb24
bychko4891 <bychko4891@gmail.com>
2023-07-14 20:43:15 +0300

    Виправлення 4 фрагмента занять, на мобільному проблемно перетаскувати, тому довелось зробити по кліку мишкою.

--- src/main/java/com/example/learnenglish/service/TranslationPairService.java
+++ src/main/java/com/example/learnenglish/service/TranslationPairService.java
@@ -59,8 +59,7 @@ public class TranslationPairService {
 
     private DtoTranslationPairToUI translationPairConvertToDtoUserText(TranslationPair translationPair) {
         DtoTranslationPairToUI dtoTranslationPairToUI = convertToDTO(translationPair);
-//        int generateNumber = new Random().nextInt(1, 5);
-        int generateNumber = 4;
+        int generateNumber = new Random().nextInt(1, 5);
         dtoTranslationPairToUI.setFragment("Fragment " + generateNumber);
         if (generateNumber == 4) {
             List<String> engTextList = new ArrayList<>(Arrays.asList(dtoTranslationPairToUI.getEngText().replaceAll("\\?+", "").split(" ")));
@@ -89,8 +88,7 @@ public class TranslationPairService {
             dtoTranslationPairToUI.setUkrText(translationPair.getUkrText());
         }
         dtoTranslationPairToUI.setEngText(translationPair.getEngText());
-//        int generateNumber = new Random().nextInt(1, 5);
-        int generateNumber = 4;
+        int generateNumber = new Random().nextInt(1, 5);
         dtoTranslationPairToUI.setFragment("Fragment " + generateNumber);
         if (generateNumber == 4) {
             List<String> engTextList = new ArrayList<>(Arrays.asList(dtoTranslationPairToUI.getEngText().replaceAll("\\?+", "").split(" ")));
