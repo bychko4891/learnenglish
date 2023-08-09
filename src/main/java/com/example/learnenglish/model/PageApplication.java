e7b5316323abfe13920853d3a0ed9c9bc6d620f2
Anatolii <106445100+bychko4891@users.noreply.github.com>
2023-08-09 23:32:52 +0300

    UserWordLessonProgress

--- src/main/java/com/example/learnenglish/model/PageApplication.java
+++ src/main/java/com/example/learnenglish/model/PageApplication.java
@@ -1,21 +1,28 @@
 package com.example.learnenglish.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "pages_application")
 public class PageApplication implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Long id;
+
     @Column(name = "name_page")
     private String namePage;
+
     @Column
     private String address;
-//    @OneToOne(mappedBy = "pageApplication", cascade = {CascadeType.PERSIST, CascadeType.REFRESH})
+
     @OneToOne(mappedBy = "pageApplication")
     private TextOfAppPage textOfAppPage;
 
@@ -28,37 +35,5 @@ public class PageApplication implements Serializable {
     public PageApplication(Long id, String namePage) {
         this.id = id;
         this.namePage = namePage;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNamePage() {
-        return namePage;
-    }
-
-    public void setNamePage(String namePage) {
-        this.namePage = namePage;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String adress) {
-        this.address = adress;
-    }
-
-    public TextOfAppPage getTextOfAppPage() {
-        return textOfAppPage;
-    }
-
-    public void setTextOfAppPage(TextOfAppPage textOfAppPage) {
-        this.textOfAppPage = textOfAppPage;
     }
 }
