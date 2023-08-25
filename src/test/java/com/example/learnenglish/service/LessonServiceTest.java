ecef37f0f8d6702c4a014008a86b6304f1978c83
arturhasparian <a.hasparian@gmail.com>
2023-08-26 00:26:46 +0300

    Refactor LessonServiceTest

--- src/test/java/com/example/learnenglish/service/LessonServiceTest.java
+++ src/test/java/com/example/learnenglish/service/LessonServiceTest.java
@@ -2,6 +2,8 @@ package com.example.learnenglish.service;
 
 import com.example.learnenglish.model.Lesson;
 import com.example.learnenglish.repository.LessonRepository;
+import com.example.learnenglish.responsemessage.Message;
+import com.example.learnenglish.responsemessage.ResponseMessage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -47,7 +49,29 @@ class LessonServiceTest {
     }
 
     @Test
-    @Disabled
-    void name() {
+    void saveLesson() {
+        var id = 1L;
+        var name = "Lesson1";
+        var lesson = new Lesson();
+        lesson.setId(id);
+        lesson.setName(name);
+        when(lessonRepository.save(lesson)).thenReturn(lesson);
+
+        var message = Message.SUCCESSADDBASE;
+
+        var responseMessage = new ResponseMessage(message);
+
+        ResponseMessage expectedLesson = lessonService.saveLesson(lesson);
+
+        assertEquals(expectedLesson, responseMessage);
+
+    }
+
+    @Test
+    void countLesson() {
+        var longest = 1L;
+        when(lessonRepository.count()).thenReturn(longest);
+        var expected = lessonService.countLessons();
+        assertEquals(expected, longest);
     }
 }
