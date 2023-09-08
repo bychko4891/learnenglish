994ff1239c05905effe92ccd039d9b10b6e9432c
arturhasparian <a.hasparian@gmail.com>
2023-09-08 23:52:30 +0300

    TranslationPairServiceTest. Created 2 tests

--- src/test/java/com/example/learnenglish/service/TranslationPairServiceTest.java
+++ src/test/java/com/example/learnenglish/service/TranslationPairServiceTest.java
@@ -1,21 +1,58 @@
 package com.example.learnenglish.service;
 
+/**
+ * @author: Artur Hasparian
+ * Application Name: Learn English
+ * Description: Integration test
+ * GitHub source code: https://github.com/bychko4891/learnenglish
+ */
+
+import com.example.learnenglish.repository.TranslationPairRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class TranslationPairServiceTest {
 
-    @Test
-    void findByCountTranslationPairInLesson() {
+    @Mock
+    private TranslationPairRepository translationPairRepository;
+    @Mock
+    private UserService userService;
+    @InjectMocks
+    private TranslationPairService translationPairService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        translationPairService = new TranslationPairService(translationPairRepository, userService);
     }
 
     @Test
-    void pairForLesson() {
+    void findByCountTranslationPairInLesson() {
+        var userId = 1L;
+        var lessonId = 1L;
+
+        when(translationPairRepository.countTranslationPairByUserIdAndLessonId(userId, lessonId)).thenReturn(userId);
+
+        var result = translationPairService.findByCountTranslationPairInLesson(1L, 1L);
+
+        assertEquals(userId,result);
     }
 
     @Test
     void existsByEngTextAndUkrText() {
+        var expectedText = "Expected text";
+
+        when(translationPairRepository.existsByEngTextAndUkrText(expectedText, 2L, 1L)).thenReturn(true);
+
+        var result = translationPairService.existsByEngTextAndUkrText(expectedText, 2L, 1L);
+
+        assertTrue(result);
     }
 
     @Test
