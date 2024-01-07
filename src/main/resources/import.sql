d581d65cb443d231c33ece719bd8a7b021c80af0
anatolii <bychko4891@gmail.com>
2024-01-07 19:32:05 +0200

    old version

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-SET client_encoding = 'UTF8';
-insert into lesson(id) values ('1');
-insert into lesson(id) values ('2');
+-- SET client_encoding = 'UTF8';
+-- insert into lesson(id) values ('1');
+-- insert into lesson(id) values ('2');
